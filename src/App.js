de573c622a49f5f5a8a542ead8c00c7176bb587f
ASUS <Yehor Kholodiuk>
2024-01-15 17:46:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [userChoice, setUserChoice] = useState('')
+    const [compChoice, setCompChoice] = useState('')
+    const [result, setResult] = useState('')
+
+    const choices = ['rock', 'paper', 'scissors']
+
+    const randomChoice = () => {
+        const randomIndex = Math.floor(Math.random() * choices.length)
+        return choices[randomIndex]
+    }
+
+    const whoIsWinner = (user, computer) => {
+
+        if (user === computer) return 'Draw!';
+
+        if (
+            (user === 'rock' && computer === 'scissors') ||
+            (user === 'scissors' && computer === 'paper') ||
+            (user === 'paper' && computer === 'rock')
+        ) {
+            return "You win!";
+        } else {
+
+            return 'Computer win!'
+        }
+    }
+    const game = (choice) => {
+        const compChoice = randomChoice();
+        setUserChoice(choice)
+        setCompChoice(compChoice)
+        setResult(whoIsWinner(choice, compChoice))
+    }
+    return (
+        <div className="App">
+            <h1>Rock, scissors, paper</h1>
+            <div>
+                <button onClick ={() => game('rock')}>Rock</button>
+                <button onClick ={() => game('scissors')}>Scissors</button>
+                <button onClick ={() => game('paper')}>Paper</button>
+
+            </div>
+            <div>
+                <p> your choice: {userChoice}</p>
+                <p>Computer Choice: {compChoice}</p>
+                <p>Result:{result}</p>
+
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
